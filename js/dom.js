b234795f7cf2298ad3470705fd4d24e02a8572e2
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-16 08:19:24 +0600

    first commit

---
+++ js/dom.js
@@ -0,0 +1,54 @@
+//document.getElementById('fruits-title').style.color = ''
+
+// document.getElementById('fruits-title').style.backgroundColor = 'red'
+// document.getElementById('fruits-title').style.padding = '20px'
+
+// const title = document.getElementById('fruits-title')
+
+// title.setAttribute( 'title', 'tooltip set by javascript')
+
+// const section = document.querySelectorAll('section')
+// for(const secton of section){
+//     secton.style.border = ' 2px solid steelblue'
+//     secton.style.marginBottom = ' 5px'
+//     secton.style.borderRadius = ' 10px'
+//     secton.style.textAlign = 'center'
+    
+    
+// }
+
+// const placesContainer = document.getElementsByClassName('places-container')
+// console.log(placesContainer);
+// console.log(placesContainer[0]);
+// console.log(placesContainer[0].childNodes);
+
+
+
+
+
+// const sum = document.createElement('p').innerText = 'Brand New place to go' 
+// console.log(sum);
+
+// const rakib = document.createElement('h1')
+// console.log(rakib);
+
+// const span = document.createElement('span')
+// console.log(span);
+
+// const li = document.createElement('li')
+// console.log(li);
+
+
+const placesUL = document.createElement('li')
+
+placesUL.innerText = ' Breand New plaes to go '
+
+const ulconstainer = document.querySelector('.placesUL')
+ulconstainer.appendChild(placesUL)
+console.log(ulconstainer);
+
+
+
+
+
+
